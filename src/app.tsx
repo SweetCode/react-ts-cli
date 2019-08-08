936b4e7a0bf1ceb37e2e01a79cee2814f146bbb0
沈文韬 <shenwt@cnlemon.net>
2019-08-08 14:23:27 +0800

    commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
-interface ssHlloProps { compiler: string; framework: string; }
+interface IHlloProps { compiler: string; framework: string; }
 
-export default (props: ssHlloProps) => <h1>Hello world!</h1>;
+export default (props: IHlloProps) => <h1>Hello world!</h1>;
